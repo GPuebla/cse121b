aae4ae1bd15ece86c69b0c3177bb45277cc396aa
GPuebla <gabrielpuebla16@gmail.com>
2023-09-23 11:44:31 -0300

    creating variables and innner elements into DOM

--- Scripts/w02-task.js
+++ Scripts/w02-task.js
@@ -4,18 +4,21 @@
 
 /* Step 2 - Variables */
 
-
-
+let fullName = "Gabriel Puebla";
+let currentDate = new Date();
+let currentYear = currentDate.getFullYear();
+let profilePicture = "Images\my_photo.jpeg"
 
 /* Step 3 - Element Variables */
 
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = ocument.querySelector('#year');
+const imageElement = ocument.querySelector("#img [src='images/my_photo.jpeg']");
 
 /* Step 4 - Adding Content */
 
-
+nameElement.innerHTML(`<strong>${fullName}</strong>`);
 
 
 
