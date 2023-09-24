856fc04208b90cc83425c6b69cb82b52cb61f259
GPuebla <gabrielpuebla16@gmail.com>
2023-09-24 00:22:12 -0300

    last commit task-2

--- Scripts/w02-task.js
+++ Scripts/w02-task.js
@@ -7,27 +7,36 @@
 let fullName = "Gabriel Puebla";
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
-let profilePicture = "Images\my_photo.jpeg"
+let profilePicture = "Images/my_photo.jpeg"
 
 /* Step 3 - Element Variables */
 
 const nameElement = document.getElementById("name");
 const foodElement = document.getElementById('food');
 const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector("img[src='#']");
-console.log(imageElement);
+const imageElement = document.querySelector("#my_image");
+console.log(profilePicture);
 
 /* Step 4 - Adding Content */
+
 nameElement.innerHTML =`<strong>${fullName}</strong>`;
 yearElement.textContent = `${currentYear}`;
-imageElement.setAttribute("src",`${profilePicture}`);
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('ref',`Profile image of ${fullName}`);
 
 
 
 
 /* Step 5 - Array */
-
-
+let favoriteFoods = ["Hot dog","Hamburger","Fries","Asado"];
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+let favFood = "Soup";
+favoriteFoods.push(favFood);
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
 
 
 
