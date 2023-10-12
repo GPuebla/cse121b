640e60a4961f2ca7ea560275798b83d6477685cd
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-10-12 16:51:48 -0300

    updating code task w04

---
+++ scripts/w04-task.js
@@ -0,0 +1,46 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let  myProfile = {
+    name: "Gabriel Puebla",
+    photo: "images/my_photo.jpeg",
+    favoriteFoods: ["Spaghetti bolognese","Beef burger", "Zucchini fritters","Asado","Lasagne","Chiken with potato","Spinach and ricotta cannelloni"],
+    hobbies: ["Learning new things", "Play piano","Cooking","Listening to music"],
+    placesLived: [],
+};
+
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLive.push(
+    {
+        place:"San Rafale, Mendoza",
+        length: "18 years"
+    },
+    {
+        place:"Belem, Brasil",
+        length: "2 years"
+    },
+    {
+        place:"Maipu, Mendoza",
+        length: "6 years"
+    }
+
+)
+
+/* DOM Manipulation - Output */
+document.querySelector("#name").textContent = "AAAAAAAAAA";
+
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
