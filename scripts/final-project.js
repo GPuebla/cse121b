aa3a7043f8cadc052e83f8ef62a3dc1a68d619d6
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-10-23 10:16:50 -0300

    test api

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -5,19 +5,26 @@ inputElement = document.querySelector('#input');
 
 const AddData = (dataBooks) =>{
 
+    reset();
     console.log('books',dataBooks);
-
-    booksElement.innerHTML = dataBooks;
 
     dataBooks.forEach(book => {
         const bookH3 = document.createElement('h3')
         bookH3.style.margin = '0'
         const bookH4 = document.createElement('h4');
+        const bookImg = document.createElement('img');
         console.log('book',book); 
         bookH3.innerHTML = book.title;
+        let isbnNum = book.isbn["0"]
         bookH4.innerHTML = book.first_publish_year;
+        let srcLink = `http://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`
+        bookImg.setAttribute('src',srcLink);
+
         booksElement.append(bookH3);
         booksElement.append(bookH4);
+        booksElement.append(bookImg);
+       // booksElement.append(document.createElement(h5).innerHTML = isbnNum);
+
     });
     
 
@@ -31,5 +38,9 @@ const getData = async () => {
     AddData(data.docs);
 
 
+};
+
+const reset = () =>{
+    booksElement.innerHTML = "";
 };
 
