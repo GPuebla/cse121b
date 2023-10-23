2f4cfb34bd6c4057a0d5a73858bd7891c14c5123
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-10-23 08:08:28 -0300

    testing API

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -1,28 +1,35 @@
-booksElement = document.querySelector('#books');
+let data = [];
 
-const getTemples = async () => {
-    const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json")
-    templeList = await response.json();
-    console.log(response);
+booksElement = document.querySelector('#books');
+inputElement = document.querySelector('#input');
+
+const AddData = (dataBooks) =>{
+
+    console.log('books',dataBooks);
+
+    booksElement.innerHTML = dataBooks;
+
+    dataBooks.forEach(book => {
+        const bookH3 = document.createElement('h3')
+        bookH3.style.margin = '0'
+        const bookH4 = document.createElement('h4');
+        console.log('book',book); 
+        bookH3.innerHTML = book.title;
+        bookH4.innerHTML = book.first_publish_year;
+        booksElement.append(bookH3);
+        booksElement.append(bookH4);
+    });
+    
+
+}
+
+
+const getData = async () => {
+    const response = await fetch("http://openlibrary.org/search.json?q="+inputElement.value)
+    data = await response.json();
+    
+    AddData(data.docs);
+
+
 };
 
-let base64 = require('base-64');
-
-let url = 'http://eu.httpbin.org/basic-auth/user/passwd';
-let username = 'user';
-let password = 'passwd';
-
-let headers = new Headers();
-
-//headers.append('Content-Type', 'text/json');
-headers.append('Authorization', 'Basic' + base64.encode(username + ":" + password));
-
-fetch(url, {method:'GET',
-        headers: headers,
-        //credentials: 'user:passwd'
-       })
-.then(response => response.json())
-.then(json => console.log(json));
-//.done();
-
-getTemples();
