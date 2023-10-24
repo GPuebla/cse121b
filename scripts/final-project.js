c9331dfebc7b6bbf0dae43f6a337aa459db34348
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-10-24 01:44:22 -0300

    updating code final proyect

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -5,28 +5,43 @@ inputElement = document.querySelector('#input');
 
 const AddData = (dataBooks) =>{
 
-    reset();
-    console.log('books',dataBooks);
+        reset();
+        console.log('books',dataBooks);
 
-    dataBooks.forEach(book => {
-        const bookH3 = document.createElement('h3')
-        bookH3.style.margin = '0'
-        const bookH4 = document.createElement('h4');
-        const bookImg = document.createElement('img');
-        console.log('book',book); 
-        bookH3.innerHTML = book.title;
+        dataBooks.forEach(book => {
+            
         let isbnNum = book.isbn["0"]
-        bookH4.innerHTML = book.first_publish_year;
-        let srcLink = `https://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`
-        bookImg.setAttribute('src',srcLink);
+        const colElement = document.createElement('div');
+        colElement.className = 'col';
 
-        booksElement.append(bookH3);
-        booksElement.append(bookH4);
-        booksElement.append(bookImg);
-       // booksElement.append(document.createElement(h5).innerHTML = isbnNum);
+        const divCard = document.createElement('div')
+        divCard.className = "card";
+        divCard.style.width = "20 px";
+        divCard.style.width = "100 px";
+        const bookImg2 = document.createElement('img');
+        bookImg2.className = "card-img-top"
+        bookImg2.style.height = "50 px";
+        bookImg2.src = `https://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`
+
+        const divCardBody = document.createElement('div')
+        divCardBody.className = "card-body";
+
+        const cardTitle = document.createElement('h5');
+        cardTitle.className = "card-title"
+        cardTitle.innerHTML = book.title[0];
+
+        const cardText = document.createElement('p');
+        cardTitle.className = "card-text"
+        cardTitle.innerHTML = book.publisher[0];
+
+        colElement.append(divCard);
+        divCard.appendChild(bookImg2);
+        divCard.appendChild(divCardBody);
+        divCardBody.appendChild(cardTitle);
+
+        booksElement.append(colElement);
 
     });
-    
 
 }
 
