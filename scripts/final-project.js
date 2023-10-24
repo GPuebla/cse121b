7620f6e9b12ecc62bcfb78cd2fab1078d609489d
Gabriel Puebla <gabrielpuebla16@gmail.com>
2023-10-24 19:52:13 -0300

    updating code of final project

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -10,7 +10,8 @@ const AddData = (dataBooks) =>{
 
         dataBooks.forEach(book => {
             
-        let isbnNum = book.isbn["0"]
+        let isbnNum = book.isbn[0];
+        
         const colElement = document.createElement('div');
         colElement.className = 'col';
 
@@ -20,8 +21,10 @@ const AddData = (dataBooks) =>{
         divCard.style.width = "100 px";
         const bookImg2 = document.createElement('img');
         bookImg2.className = "card-img-top"
-        bookImg2.style.height = "50 px";
+        bookImg2.style.height = "40 px";
+        bookImg2.style.width = "30 px";
         bookImg2.src = `https://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`
+        console.log(bookImg2.sizes);
 
         const divCardBody = document.createElement('div')
         divCardBody.className = "card-body";
@@ -34,10 +37,14 @@ const AddData = (dataBooks) =>{
         cardTitle.className = "card-text"
         cardTitle.innerHTML = book.publisher[0];
 
+        const textTest = document.createElement('h4');
+        textTest.innerHTML = bookImg2.textContent;
+
         colElement.append(divCard);
         divCard.appendChild(bookImg2);
         divCard.appendChild(divCardBody);
         divCardBody.appendChild(cardTitle);
+        divCardBody.append(textTest);
 
         booksElement.append(colElement);
 
@@ -51,8 +58,6 @@ const getData = async () => {
     data = await response.json();
     
     AddData(data.docs);
-
-
 };
 
 const reset = () =>{
