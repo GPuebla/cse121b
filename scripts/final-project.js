e70ceb623828202b5522213dba01558d2f46d1af
GPuebla <gabrielpuebla16@gmail.com>
2023-10-24 23:35:45 -0300

    updating code

--- scripts/final-project.js
+++ scripts/final-project.js
@@ -1,3 +1,4 @@
+
 let data = [];
 
 booksElement = document.querySelector('#books');
@@ -8,7 +9,7 @@ const AddData = (dataBooks) =>{
         reset();
         console.log('books',dataBooks);
 
-        dataBooks.forEach(book => {
+        dataBooks.forEach(async book => {
             
         let isbnNum = book.isbn[0];
         
@@ -21,10 +22,16 @@ const AddData = (dataBooks) =>{
         divCard.style.width = "100 px";
         const bookImg2 = document.createElement('img');
         bookImg2.className = "card-img-top"
-        bookImg2.style.height = "40 px";
-        bookImg2.style.width = "30 px";
-        bookImg2.src = `https://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`
-        console.log(bookImg2.sizes);
+        //bookImg2.style.height = "40 px";
+        //bookImg2.style.width = "30 px";
+
+        let genericImg = "https://th.bing.com/th/id/OIP.nBwIfauMfjWd0qbnifW7YgHaHa?pid=ImgDet&rs=1";
+
+        let srcImg = await checkImg(`https://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`,genericImg);
+        
+    console.log(srcImg)
+
+        bookImg2.src = srcImg;
 
         const divCardBody = document.createElement('div')
         divCardBody.className = "card-body";
@@ -57,6 +64,7 @@ const getData = async () => {
     const response = await fetch("https://openlibrary.org/search.json?q="+inputElement.value)
     data = await response.json();
     
+
     AddData(data.docs);
 };
 
@@ -64,3 +72,44 @@ const reset = () =>{
     booksElement.innerHTML = "";
 };
 
+
+//---------------------------------------------------------------------//
+
+const checkImg = (url, optionalImg) => {
+
+    // URL de la imag
+    const imageUrl = url
+
+    // Crea un nuevo objeto de imagen
+    const img = new Image();
+
+    // Define una función para ejecutar cuando la imagen se cargue
+     img.onload = function  () {
+    // Accede a las dimensiones de la imagen
+    const width = this.width;
+    const height = this.height;
+
+    // Verifica el tamaño de la imagen (por ejemplo, 100x100 píxeles)
+        if (width >= 50 && height >= 50) {
+            // La imagen tiene el tamaño deseado
+            console.log("La imagen tiene el tamaño deseado (100x100 píxeles).");
+            return url
+        } else {
+            // La imagen no tiene el tamaño deseado
+            console.log("La imagen no tiene el tamaño deseado.");
+            return optionalImg
+        }
+    };
+
+    // Define una función para ejecutar si la imagen no se puede cargar
+    img.onerror = function() {
+    console.log("Error al cargar la imagen.");
+    };
+
+    // Establece la fuente de la imagen
+    img.src = imageUrl;
+
+}
+
+
+//--------------------------------------------------------------------------------------------//
--- scripts/functions.js
+++ scripts/functions.js
@@ -1,34 +1,34 @@
 
-    const checkImg = async (url) => {
+    const checkImg = async (url, optionalImg) => {
 
-        // URL de la imagen
-    const imageUrl = `https://covers.openlibrary.org/b/isbn/${isbnNum}-M.jpg`;
+        // URL de la imag
+        const imageUrl = url
 
-    // Crea un nuevo objeto de imagen
-    const img = new Image();
+        // Crea un nuevo objeto de imagen
+        const img = new Image();
 
-    // Define una función para ejecutar cuando la imagen se cargue
-    img.onload = function() {
-    // Accede a las dimensiones de la imagen
-    const width = this.width;
-    const height = this.height;
+        // Define una función para ejecutar cuando la imagen se cargue
+        img.onload = function() {
+        // Accede a las dimensiones de la imagen
+        const width = this.width;
+        const height = this.height;
 
-    // Verifica el tamaño de la imagen (por ejemplo, 100x100 píxeles)
-    if (width === 100 && height === 100) {
-        // La imagen tiene el tamaño deseado
-        console.log("La imagen tiene el tamaño deseado (100x100 píxeles).");
-    } else {
-        // La imagen no tiene el tamaño deseado
-        console.log("La imagen no tiene el tamaño deseado.");
+        // Verifica el tamaño de la imagen (por ejemplo, 100x100 píxeles)
+        if (width >= 50 && height >= 50) {
+            // La imagen tiene el tamaño deseado
+            console.log("La imagen tiene el tamaño deseado (100x100 píxeles).");
+        } else {
+            // La imagen no tiene el tamaño deseado
+            console.log("La imagen no tiene el tamaño deseado.");
+        }
+        };
+
+        // Define una función para ejecutar si la imagen no se puede cargar
+        img.onerror = function() {
+        console.log("Error al cargar la imagen.");
+        };
+
+        // Establece la fuente de la imagen
+        img.src = imageUrl;
     }
-    };
-
-    // Define una función para ejecutar si la imagen no se puede cargar
-    img.onerror = function() {
-    console.log("Error al cargar la imagen.");
-    };
-
-    // Establece la fuente de la imagen
-    img.src = imageUrl;
-}
 
